c4b8fa322b5d8c4b4bee783a68926ff040378bf3
fguan0529 <guan.fanny@gmail.com>
2019-02-21 01:42:02 -0800

    Anchor scroll fixes. Base href fix attempt.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 
+
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'enabled',
-    anchorScrolling: 'enabled',
-    scrollOffset: [0, 75] // [x, y]
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
