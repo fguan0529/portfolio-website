6b7bd5794e56e0182f8acbc0965dd73e1ca3a265
fguan0529 <guan.fanny@gmail.com>
2019-02-27 15:57:07 -0800

    Added tooltip for email copied status message.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -24,10 +24,9 @@ export class ContactComponent implements OnInit {
   resumeLinkSrc = '';
 
   copyEmail() {
-    let emailBox = document.createElement('textarea');
+    const emailBox = document.createElement('textarea');
     emailBox.value = 'guan.fanny@gmail.com';
     document.body.appendChild(emailBox);
-    emailBox.focus();
     emailBox.select();
     document.execCommand('copy');
     document.body.removeChild(emailBox);
