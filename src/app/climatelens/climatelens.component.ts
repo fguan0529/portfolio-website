94aa059a1479e97400483abed77c4188e0d04c3b
fguan0529 <guan.fanny@gmail.com>
2019-03-05 17:46:22 -0800

    Microsite details 60%

--- src/app/climatelens/climatelens.component.ts
+++ src/app/climatelens/climatelens.component.ts
@@ -10,28 +10,29 @@ import { PROJECTS } from '../project-list';
 export class ClimatelensComponent implements OnInit {
 
   climateLens: Project = PROJECTS[0];
+  path = this.climateLens.imgDir;
 
-  logoImg = this.climateLens.imgDir + 'logo.png';
-  leanCanvasImg = this.climateLens.imgDir + 'leancanvas.png';
-  valuePropImg = this.climateLens.imgDir + 'valueprop.png';
-  compAnaImg1 = this.climateLens.imgDir + 'compana_1.png';
-  compAnaImg2 = this.climateLens.imgDir + 'compana_2.png';
-  compAnaImg3 = this.climateLens.imgDir + 'compana_3.png';
-  userJourneyImg = this.climateLens.imgDir + 'userjourney.png';
-  iconImg = this.climateLens.imgDir + 'icon.png';
-  splashImg1 = this.climateLens.imgDir + 'splash_1.png';
-  splashImg2 = this.climateLens.imgDir + 'splash_2.png';
-  onboardImg1 = this.climateLens.imgDir + 'onboard_1.png';
-  onboardImg2 = this.climateLens.imgDir + 'onboard_2.png';
-  onboardImg3 = this.climateLens.imgDir + 'onboard_3.png';
-  onboardImg4 = this.climateLens.imgDir + 'onboard_4.png';
-  mapImg = this.climateLens.imgDir + 'map.png';
-  arImg1 = this.climateLens.imgDir + 'ar_1.png';
-  arImg2 = this.climateLens.imgDir + 'ar_2.png';
-  participateImg = this.climateLens.imgDir + 'participate.png';
-  orgImg1 = this.climateLens.imgDir + 'organizations_1.png';
-  orgImg2 = this.climateLens.imgDir + 'organizations_2.png';
-  pledgesImg = this.climateLens.imgDir + 'pledges.png';
+  logoImg = this.path + 'logo.png';
+  leanCanvasImg = this.path + 'leancanvas.png';
+  valuePropImg = this.path + 'valueprop.png';
+  compAnaImg1 = this.path + 'compana_1.png';
+  compAnaImg2 = this.path + 'compana_2.png';
+  compAnaImg3 = this.path + 'compana_3.png';
+  userJourneyImg = this.path + 'userjourney.png';
+  iconImg = this.path + 'icon.png';
+  splashImg1 = this.path + 'splash_1.png';
+  splashImg2 = this.path + 'splash_2.png';
+  onboardImg1 = this.path + 'onboard_1.png';
+  onboardImg2 = this.path + 'onboard_2.png';
+  onboardImg3 = this.path + 'onboard_3.png';
+  onboardImg4 = this.path + 'onboard_4.png';
+  mapImg = this.path + 'map.png';
+  arImg1 = this.path + 'ar_1.png';
+  arImg2 = this.path + 'ar_2.png';
+  participateImg = this.path + 'participate.png';
+  orgImg1 = this.path + 'organizations_1.png';
+  orgImg2 = this.path + 'organizations_2.png';
+  pledgesImg = this.path + 'pledges.png';
 
 
 
--- src/app/investment-microsite/investment-microsite.component.ts
+++ src/app/investment-microsite/investment-microsite.component.ts
@@ -10,6 +10,15 @@ import {PROJECTS} from '../project-list';
 export class InvestmentMicrositeComponent implements OnInit {
 
   microsite: Project = PROJECTS[1];
+  path = this.microsite.imgDir;
+
+  designProcessImg = this.path + 'design_process.png';
+  personaImg1 = this.path + 'Thomas_Investor.png';
+  personaImg2 = this.path + 'Nancy_Investor.png';
+  personaImg3 = this.path + 'Roger_Institution.png';
+  personaImg4 = this.path + 'David_Advisor.png';
+  flowImg = this.path + 'flow.png';
+
 
   constructor() { }
 
