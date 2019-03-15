98fd411dc27ea19b94d26cd33bc8e61a6745c7ea
fguan0529 <guan.fanny@gmail.com>
2019-03-15 16:34:48 -0700

    added fadeins for home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn } from 'ng-animate';
+import {transition, trigger, useAnimation} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeIn', [transition('* => *', useAnimation(fadeIn))])
+  ],
 })
 export class HomeComponent implements OnInit {
+  fadeIn: any;
 
   profileImg = './assets/img/photo.jpg';
 
