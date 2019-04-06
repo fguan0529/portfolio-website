c1378282c03a915ae0baad21879b5457bcc573ad
fguan0529 <guan.fanny@gmail.com>
2019-04-06 14:41:11 -0700

    Added slide in animations. New profile photo.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {NgsRevealConfig} from 'ngx-scrollreveal';
+import { NgsRevealConfig } from 'ngx-scrollreveal';
+import { slideInRight, slideInLeft } from 'ng-animate';
+import {transition, trigger, useAnimation} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('slideInRight', [transition('* => *', useAnimation(slideInRight))]),
+    trigger('slideInLeft', [transition('* => *', useAnimation(slideInLeft))]),
+  ]
 })
 export class HomeComponent implements OnInit {
   profileImg = './assets/img/photo.jpg';
+  profileImgAlt = './assets/img/mushu_fanny.svg';
+
+  slideInRight: any;
+  slideInLeft: any;
 
   constructor(config: NgsRevealConfig) {
     config.duration = 800;
